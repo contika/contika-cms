62c843617bf33bb1381a251c519a4c0d94e78694
Alvin Bakker <alvin@spin.cw>
2019-02-07 19:34:06 -0400

    Cleaning up and setting paths

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,21 @@
-<?php
-
-namespace App\Providers;
+<?php namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+class AppServiceProvider extends ServiceProvider {
+
+    public $version = "0.0.1";
+
+    public function boot() {
+        $presets = [
+            'version' => $this->version
+            ];
+        config($presets);
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+
+    public function register() {
+
     }
+
 }
