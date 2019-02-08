53f99760bf1e23905aa70f131798ecc413b24c01
Alvin Bakker <alvin@spin.cw>
2019-02-07 20:08:11 -0400

    Added user auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,12 +1,14 @@
 <?php namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider {
 
     public $version = "0.0.1";
 
     public function boot() {
+        Schema::defaultStringLength(191);
         $presets = [
             'version' => $this->version
             ];
